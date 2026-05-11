e545665aef4bf15f1e02a1dcc4c6b010765a216b
Marco Tonelli <marco87developer@gmail.com>
2026-05-11 08:59:36 +0200

    feat(exceptions): add fromJson and fromMap format exception messages

--- lib/src/exceptions/exception_messages.dart
+++ lib/src/exceptions/exception_messages.dart
@@ -1,4 +1,21 @@
 /// Returns a string with the message to use in the [FormatException] thrown
+/// when the JSON string passed to the `fromJson` constructor is invalid.
+///
+String fromJsonFormatExceptionMessage(
+  final String classOrEnumName,
+  final String formattedString,
+) =>
+    '$classOrEnumName.fromJson: the JSON string “$formattedString” is invalid.';
+
+/// Returns a string with the message to use in the [FormatException] thrown
+/// when the map value passed to the `fromMap` constructor is invalid.
+///
+String fromMapFormatExceptionMessage(
+  final String classOrEnumName,
+  final String key,
+) => "$classOrEnumName.fromMap: map['$key'] value is invalid.";
+
+/// Returns a string with the message to use in the [FormatException] thrown
 /// when the string passed to the `parse` constructor is invalid.
 ///
 String parseFormatExceptionMessage(
