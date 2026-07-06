20c80aa8ef0e17e4cc7d512f7bc9723d06b00531
Marco Tonelli <marco87developer@gmail.com>
2026-07-06 15:46:27 +0200

    refactor(SplayTreeSetExtension): use iterable helpers for splay tree set comparisons
    Removes custom duplicate implementation of iterable comparison inside
    splay_tree_set_extension.dart and delegates to elementCompareIterables
    and elementCompareIterablesReversed from iterable_helpers.dart.

--- lib/src/extensions/splay_tree_set_extension.dart
+++ lib/src/extensions/splay_tree_set_extension.dart
@@ -1,25 +1,6 @@
 import 'dart:collection';
 
-/// Compares two [Iterable]s of [Comparable] elements lexicographically.
-///
-/// Iterates both sequences in lock-step and returns the comparison result of
-/// the first pair of elements that differs. Returns `0` if all paired elements
-/// are equal.
-///
-int _compareIterables<T extends Comparable<T>>(
-  final Iterable<T> a,
-  final Iterable<T> b,
-) {
-  final Iterator<T> ai = a.iterator;
-  final Iterator<T> bi = b.iterator;
-  while (ai.moveNext() && bi.moveNext()) {
-    final int elementComparison = ai.current.compareTo(bi.current);
-    if (elementComparison != 0) {
-      return elementComparison;
-    }
-  }
-  return 0;
-}
+import 'package:scheduler/src/helpers/iterable_helpers.dart';
 
 /// A [SplayTreeSet] extension that provides methods for comparing them
 /// lexicographically.
@@ -27,30 +8,12 @@ int _compareIterables<T extends Comparable<T>>(
 extension SplayTreeSetExtension<T extends Comparable<T>> on SplayTreeSet<T> {
   /// Compares this [SplayTreeSet] with [other] lexicographically.
   ///
-  int compareTo(final SplayTreeSet<T> other) {
-    if (identical(this, other)) {
-      return 0;
-    }
-    return switch ((isEmpty, other.isEmpty)) {
-      (true, true) => 0,
-      (true, false) => -1,
-      (false, true) => 1,
-      _ => _compareIterables(this, other),
-    };
-  }
+  int compareTo(final SplayTreeSet<T> other) =>
+      elementCompareIterables(this, other);
 
   /// Compares this [SplayTreeSet] with [other] lexicographically, but starting
   /// from the last element.
   ///
-  int compareToReversed(final SplayTreeSet<T> other) {
-    if (identical(this, other)) {
-      return 0;
-    }
-    return switch ((isEmpty, other.isEmpty)) {
-      (true, true) => 0,
-      (true, false) => -1,
-      (false, true) => 1,
-      _ => _compareIterables(toList().reversed, other.toList().reversed),
-    };
-  }
+  int compareToReversed(final SplayTreeSet<T> other) =>
+      elementCompareIterablesReversed(this, other);
 }
